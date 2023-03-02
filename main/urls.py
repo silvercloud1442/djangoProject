b9987ea833f053523bf20810642238c3b7404d12
user <kudakovazam@yandex.ru>
2023-03-02 10:28:08 +0300

    jija

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-"""djangoProject URL Configuration
+"""main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tours.views import index, tours
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('tours/', tours, name='tours')
 ]
