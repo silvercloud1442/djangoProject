c4220c062a3c6427144cf1d3a4cf3d21d3fd3e58
user <kudakovazam@yandex.ru>
2023-03-03 13:30:56 +0300

    jija 3.0

--- tours/views.py
+++ tours/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'tours/index.html')
+    context = {
+                'title': 'INDEX PAGE'
+    }
+    return render(request, 'tours/index.html', context=context)
 
 def tours(requset):
-    return render(requset, 'tours/tours.html')
+    tours = [
+        {'name': 'name 1', 'description': 'description 1', 'price': 100},
+        {'name': 'name 2', 'description': 'description 2', 'price': 200},
+        {'name': 'name 3', 'description': 'description 3', 'price': 300},
+             ]
+    context = {
+                'title': 'TOURS PAGE',
+                'tours': tours
+    }
+    return render(requset, 'tours/tours.html', context=context)
