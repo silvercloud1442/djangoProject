7ba05f78ce2267aca7fa8b9d9d2764c7bdec6dee
user <kudakovazam@yandex.ru>
2023-03-10 12:03:15 +0300

    forms 1

--- tours/forms.py
+++ tours/forms.py
@@ -1,9 +1,22 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from tours.models import *
 
-class AddTourForm(forms.Form):
-    name = forms.CharField(max_length=128, label='Название')
-    slug = forms.SlugField(max_length=255, label='URL')
-    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Описание')
-    price = forms.IntegerField(label='Цена')
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Не выбрано')
+class AddTourForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].empty_label = 'Не выбрано'
+    class Meta:
+        model = Tour
+        fields = '__all__'
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'input-group-text'}),
+            'description': forms.Textarea(attrs={'cols': 60, 'rows': 5})
+        }
+
+    def clead_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('Длина превышает 200 символов')
+        return title
--- tours/views.py
+++ tours/views.py
@@ -1,41 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 from tours.forms import AddTourForm
 from tours.models import *
 
+
 def index(request):
     context = {
-                'title': 'INDEX PAGE'
+        'title': 'INDEX PAGE'
     }
     return render(request, 'tours/index.html', context=context)
 
-def tours(requset):
 
+def tours(requset):
     context = {
-                'title': 'TOURS PAGE',
-                'tours': Tour.objects.all(),
-                'category': Category.objects.all()
+        'title': 'TOURS PAGE',
+        'tours': Tour.objects.all(),
+        'category': Category.objects.all()
     }
     return render(requset, 'tours/tours.html', context=context)
 
+
+@csrf_exempt
 def add_tour(request):
     if request.method == 'POST':
-        form = AddTourForm(request.POST)
+        form = AddTourForm(request.POST, request.FILES)
         if form.is_valid():
-            print(form.cleaned_data)
+            form.save()
+            return redirect('index')
     else:
         form = AddTourForm()
     context = {
-                'title': 'ADD TOUR',
-                'form': form
+        'title': 'ADD TOUR',
+        'form': form
     }
-    return render(request, 'tours/add_page.html', context=context
-                  )
+    return render(request, 'tours/add_tour.html', context=context)
+
 
 def show_tour(request, tour_slug):
     tour = Tour.objects.get(slug=tour_slug)
     context = {
-                'title': tour.name,
-                'tour': tour
+        'title': tour.name,
+        'tour': tour
     }
     return render(request, 'tours/show_tour.html', context=context)
