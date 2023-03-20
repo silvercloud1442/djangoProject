cc7330af7ebca2aec95c4ef3c98c8907c59fc276
ПК-8 <kudakovazam@yandex.ru>
2023-03-20 11:15:50 +0300

    admin custom

--- tours/admin.py
+++ tours/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from tours.models import *
 
 class TourAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'image', 'category')
+    list_display = ('name', 'description', 'price', 'get_html_image', 'category')
     list_display_links = ('name',)
     search_fields = ('name', 'description')
     prepopulated_fields = {'slug': ('name',)}
+    fields = ('name', 'slug', 'description', 'price', 'image', 'get_html_image', 'category')
+    readonly_fields = ('get_html_image', )
+    save_on_top = True
+
+    def get_html_image(self, object):
+        if object.image:
+            return mark_safe(f"<img src='{object.image.url}' width=50>")
+
+    get_html_image.short_description = 'Изображение'
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
@@ -16,3 +28,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tour, TourAdmin)
+
+admin.site.site_header = 'Кто админ, тот чёрт.'
