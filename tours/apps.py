65ffb2bc74d586cf713fc6014a68521695791718
ПК-8 <kudakovazam@yandex.ru>
2023-04-27 12:05:03 +0300

    added

--- tours/apps.py
+++ tours/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ToursConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tours"
+    verbose_name = 'База(кормит) данных'
